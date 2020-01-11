06b165c1a0e23924309645f2c8896cd0113da964
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-01-11 08:31:04 +0800

    change error handling location

--- get_episode.py
+++ get_episode.py
@@ -1,2 +1,7 @@
 def with_episode_number(episode_number):
+    try:
+        int(episode_number)
+    except ValueError:
+        raise TypeError
+
     print(f"The episode number is {episode_number}")

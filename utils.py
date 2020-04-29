c06cfb712eeb6f396c6503f57802b42b372a7dcd
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-04-29 13:45:57 +0800

    move settings function to utils

--- utils.py
+++ utils.py
@@ -13,3 +13,22 @@ def print_usage_message():
     path = f"{get_absolute_path('usage_message.txt')}"
     with open(path, 'r') as f:
         print(f.read())
+
+
+def get_settings():
+    with open("settings.txt", 'r') as f:
+        raw_file = f.read()
+
+    lines = raw_file.split("\n")
+
+    settings = dict()
+    for line in lines:
+        key, value = None, None
+        try:
+            key, value = line.split()
+        except ValueError:
+            # there is formatting whitespace
+            break
+        settings[key] = value
+
+    return settings

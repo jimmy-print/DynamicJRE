4e2d69cce5238549b0268f86d9566f1fa4fae058
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-05-11 11:36:04 +0800

    fix cleanup and save folder bug; allow no folder

--- get_episode.py
+++ get_episode.py
@@ -10,46 +10,46 @@ folder = os.path.expanduser(settings.retrieve("Save-folder"))
 
 
 def _download(download_link, episode_number, folder):
-    print(f"Downloading episode {episode_number}")
-    raw_episode = requests.get(download_link)
-    with open(f"{folder}/p{episode_number}.mp3", "wb") as f:
-        print("Writing to mp3")
-        f.write(raw_episode.content)
+    try:
+        print(f"Downloading episode {episode_number}")
+        raw_episode = requests.get(download_link)
+    except KeyboardInterrupt:
+        return
+    try:
+        with open(f"{folder}/p{episode_number}.mp3", "wb") as f:
+            print("Writing to mp3")
+            f.write(raw_episode.content)
+    except KeyboardInterrupt:
+        cleanup(episode_number)
 
 
 def with_episode_number(episode_number, folder=folder):
     url_format = "http://traffic.libsyn.com/joeroganexp/p"
 
-    try:
-        _download(f"{url_format}{episode_number}.mp3", episode_number, folder)
-    except KeyboardInterrupt:
-        cleanup(episode_number)
+    _download(f"{url_format}{episode_number}.mp3", episode_number, folder)
 
 
 def latest(folder=folder):
-    homepage = "http://podcasts.joerogan.net/"
-    response = requests.get(homepage)
-    soup = BeautifulSoup(response.text, "lxml")
-
-    latest_element = soup.find_all("div", attrs={"class": "episode odd"})[0]
-    links = latest_element.find_all("li")
-    download_link = links[2].a.get("href")
-
-    file_name = download_link.split("/")[4]
-    # Stripping with .mp3 is perfect, as the p in front also gets removed.
-    episode_number = file_name.strip(".mp3")
-
     try:
-        _download(download_link, episode_number, folder)
+        homepage = "http://podcasts.joerogan.net/"
+        response = requests.get(homepage)
+        soup = BeautifulSoup(response.text, "lxml")
+
+        latest_element = soup.find_all("div", attrs={"class": "episode odd"})[0]
+        links = latest_element.find_all("li")
+        download_link = links[2].a.get("href")
+
+        file_name = download_link.split("/")[4]
+        # Stripping with .mp3 is perfect, as the p in front also gets removed.
+        episode_number = file_name.strip(".mp3")
     except KeyboardInterrupt:
-        cleanup(episode_number)
+        return
+
+    _download(download_link, episode_number, folder)
 
 
-def cleanup(episode_number):
+def cleanup(episode_number, folder=folder):
     print("\nKeyboard interrupt detected")
     print("Commencing cleanup")
-    try:
-        os.remove(f"p{episode_number}.mp3")
-    except FileNotFoundError:
-        pass
+    os.remove(f"{folder}/p{episode_number}.mp3")
     print("Cleanup complete. Exiting.")
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -46,13 +46,15 @@ class TestSettingsSaveFolder(unittest.TestCase):
 
     def tearDown(self):
         os.remove(self.settings_file)
-        os.remove(f"{self.folder}/p{self.number}.mp3")
+        #os.remove(f"{self.folder}/p{self.number}.mp3")
+        get_episode.cleanup(self.number, folder=self.folder)
 
     def test_save_folder(self):
         settings = utils.Settings()
         settings.load("tests/settings-save-folder.txt")
         self.folder = os.path.expanduser(settings.retrieve("Save-folder"))
         get_episode._download("https://example.com", self.number, folder=self.folder)
+        print(f"{self.folder}/p{self.number}.mp3")
         self.assertTrue(os.path.isfile(f"{self.folder}/p{self.number}.mp3"))
 
 

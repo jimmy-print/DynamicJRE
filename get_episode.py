cd1ad3e4a8ab48c6089ba76a28707bb440b9024f
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-06-01 21:14:52 +0800

    add better extendable support for mma episodes and tests

--- get_episode.py
+++ get_episode.py
@@ -6,18 +6,28 @@ import utils
 
 folder = utils.get_save_folder()
 
+REGULAR = "regular"
+MMA = "mma"
+FIGHT = "fight"
+episode_types = (REGULAR, MMA, FIGHT)
+url_formats = (
+    "http://traffic.libsyn.com/joeroganexp/p{}.mp3",
+    "http://traffic.libsyn.com/joeroganexp/mmashow{}.mp3",
+)
+# Fight episodes aren't supported yet
+episode_type_url_format = dict(zip(episode_types, url_formats))
 
-class ProbablyNotRegularEpisode(Exception):
-    pass
 
-
-def _download(download_link, episode_number, folder=folder):
+def download(episode_number, episode_type, folder=folder, headers=None):
     try:
-        print(f"Downloading episode {episode_number}")
-        raw_episode = requests.get(download_link)
+        print(f"Downloading episode {episode_type} {episode_number}")
+        download_link = (
+            episode_type_url_format[episode_type].format(episode_number))
+        raw_episode = requests.get(download_link, headers=headers)
         raw_episode.raise_for_status()
-    except requests.exceptions.HTTPError:
-        raise ProbablyNotRegularEpisode
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return
     except KeyboardInterrupt:
         return
     try:
@@ -28,13 +38,14 @@ def _download(download_link, episode_number, folder=folder):
         cleanup(episode_number)
 
 
-def with_episode_number(episode_number, folder=folder):
-    url_format = "http://traffic.libsyn.com/joeroganexp/p"
+def with_episode_number(episode_number, folder=folder, headers=None):
+    # with_episode_number only works with regular episodes for now
+    download(episode_number, REGULAR, folder=folder, headers=headers)
 
-    _download(f"{url_format}{episode_number}.mp3", episode_number, folder=folder)
 
+def get_latest_episode_attributes():
+    """Returns latest episode number and type (Regular, MMA, Fight)"""
 
-def get_latest_episode_number():
     homepage = "http://podcasts.joerogan.net/"
     response = requests.get(homepage)
     soup = BeautifulSoup(response.text, "lxml")
@@ -43,30 +54,33 @@ def get_latest_episode_number():
     episode_number_element = latest_element.find("span", attrs={"class": "episode-num"})
     episode_number = episode_number_element.text.strip("#")
 
-    return episode_number
+    title_element = latest_element.find("a", attrs={"class": "ajax-permalink"})
+    title_text = title_element.get("data-title")
+    if "mma show" in title_text.lower():
+        episode_type = MMA
+    elif "fight companion" in title_text.lower():
+        episode_type = FIGHT
+    else:
+        episode_type = REGULAR
+
+    return episode_number, episode_type
 
 
-def latest(folder=folder):
+def latest(folder=folder, headers=None):
     try:
-        episode_number = get_latest_episode_number()
-        url_format = "http://traffic.libsyn.com/joeroganexp/p"
+        episode_number, episode_type = get_latest_episode_attributes()
     except KeyboardInterrupt:
         return
 
-    try:
-        _download(f"{url_format}{episode_number}.mp3", episode_number, folder=folder)
-    except ProbablyNotRegularEpisode:
-        # Now, the latest episode is either MMA show, Fight Companion, or a
-        # regular episode but with an a after p### in the url.
-        # However, the url could just be broken, if for example the way the
-        # podcasts were formatted according to their numbers changed.
-
-        url_format = "http://traffic.libsyn.com/joeroganexp/mmashow"
-
-        _download(f"{url_format}{episode_number}.mp3", episode_number, folder=folder)
+    download(episode_number, episode_type, folder=folder, headers=headers)
+    return episode_number, episode_type # For testing purposes
 
 
 def cleanup(episode_number, folder=folder):
     print("Commencing cleanup")
     os.remove(f"{folder}/p{episode_number}.mp3")
     print("Cleanup complete. Exiting.")
+
+
+if __name__ == "__main__":
+    download("0", FIGHT)
--- tests/test_get_episode.py
+++ tests/test_get_episode.py
@@ -5,46 +5,55 @@ import requests
 import get_episode
 import utils
 
+headers = {
+    "Range": "bytes=0-100"
+}
+
+number = 1000
+
+
+def make_test_request(self):
+    get_episode.download(
+            number, get_episode.REGULAR,
+            folder=self.folder, headers=headers)
+
 
 class TestSaveFolder(unittest.TestCase):
     def setUp(self):
         self.settings_file = "tests/settings-save-folder.txt"
-        self.number = 1000
         with open(self.settings_file, 'w') as f:
             f.write(f"Save-folder ~/Desktop")
 
     def tearDown(self):
         os.remove(self.settings_file)
-        get_episode.cleanup(self.number, folder=self.folder)
+        get_episode.cleanup(number, folder=self.folder)
 
     def test_save_folder(self):
         self.folder = os.path.expanduser(utils.get_save_folder(self.settings_file))
-        get_episode._download("https://example.com", self.number, folder=self.folder)
-        print(f"{self.folder}/p{self.number}.mp3")
-        self.assertTrue(os.path.isfile(f"{self.folder}/p{self.number}.mp3"))
+        print(f"{self.folder}/p{number}.mp3")
+        make_test_request(self)
+        self.assertTrue(os.path.isfile(f"{self.folder}/p{number}.mp3"))
 
 
 class TestNoSaveFolder(unittest.TestCase):
     def setUp(self):
         self.settings_file = "tests/settings-no-save-folder.txt"
-        self.number = 1000
         with open(self.settings_file, 'w') as f:
             pass
 
     def tearDown(self):
         os.remove(self.settings_file)
-        get_episode.cleanup(self.number, folder=self.folder)
+        get_episode.cleanup(number, folder=self.folder)
 
     def test_no_save_folder(self):
         self.folder = os.path.expanduser(utils.get_save_folder("tests/settings-no-save-folder.txt"))
-        get_episode._download("https://example.com", self.number, folder=self.folder)
-        self.assertTrue(os.path.isfile(f"p{self.number}.mp3"))
+        make_test_request(self)
+        self.assertTrue(os.path.isfile(f"p{number}.mp3"))
 
 
 class TestCleanup(unittest.TestCase):
     def setUp(self):
         self.settings_file = "tests/settings-cleanup.txt"
-        self.number = 1000
         with open(self.settings_file, 'w'):
             pass
 
@@ -53,16 +62,17 @@ class TestCleanup(unittest.TestCase):
 
     def test_cleanup(self):
         self.folder = os.path.expanduser(utils.get_save_folder(self.settings_file))
-        get_episode._download("https://example.com", self.number, folder=self.folder)
-        get_episode.cleanup(self.number, folder=self.folder)
-        self.assertFalse(os.path.isfile(f"p{self.number}.mp3"))
+        make_test_request(self)
+        get_episode.cleanup(number, folder=self.folder)
+        self.assertFalse(os.path.isfile(f"p{number}.mp3"))
 
 
-class TestGetLatestEpNumber(unittest.TestCase):
-    def test_get_latest_ep_number(self):
-        num = get_episode.get_latest_episode_number()
-        print(num)
-        int(num)
-        # There is no self.assertDoesNotRaise in unittest, so this will
-        # cause an error instead of a failure if the test fails, as if
-        # the test itself was broken.
+class TestGetLatestEp(unittest.TestCase):
+    def test_get_latest_ep_attrs(self):
+        episode_num, episode_type = get_episode.get_latest_episode_attributes()
+        int(episode_num)
+        self.assertTrue(episode_type, get_episode.episode_types)
+
+    def test_get_latest_ep(self):
+        episode_num_alt, __ = get_episode.latest(headers=headers)
+        get_episode.cleanup(episode_num_alt)

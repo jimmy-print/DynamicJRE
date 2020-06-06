0e84bab2f3ecece4e8fa07595cb94792a323817a
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-06-06 17:47:07 +0800

    use open() for creating settings.txt; creates in abs path

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
 
-print("--------------------")
+import utils
 
 print("Creating symbolic link")
 subprocess.run(["ln", "-s", "$(pwd)/jrep.py", "/usr/local/bin/jrep"])
 print()
 
 print("Creating settings file")
-subprocess.run(["touch", "settings.txt"])
+if os.path.isfile(utils.get_absolute_path("settings.txt")):
+    print("File exists.")
+else:
+    with open(utils.get_absolute_path("settings.txt"), 'w') as f:
+        pass
 print()
 
 print("Setup complete.")
-
-print("--------------------")

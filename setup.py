5cd6b4b1bdf6b9b614982f3598434686970dce34
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-06-10 19:49:00 +0800

    add info to setup.py and fix requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 setup(
     name="DynamicJRE",
     version="0.0",
+    author="jimmy-print",
     description="Command line podcast downloader for The JRE",
     author_email="jimmy.wang123@outlook.sg",
+    url="https://github.com/jimmy-print/DynamicJRE",
+    license="MIT",
     packages=["dynamic_jre"],
     entry_points={
         "console_scripts": ['jrep = dynamic_jre.jrep:main']
     },
-    install_requirements=["requests", "bs4"],
+    install_requires=["requests", "bs4", "lxml"],
 )

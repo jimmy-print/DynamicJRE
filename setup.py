bd6ffffec80c17b00261b9eabfb1fd1e8b085919
jimmy-print <jimmy.wang123@outlook.sg>
2020-09-11 20:45:57 +0800

    rename

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="jimmy.wang123@outlook.sg",
     url="https://github.com/jimmy-print/DynamicJRE",
     license="MIT",
-    packages=["dynamic_jre"],
+    packages=["dynamicjre"],
     entry_points={
         "console_scripts": ['jrep = dynamic_jre.jrep:main']
     },
